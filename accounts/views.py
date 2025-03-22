c1c61f735474764489d11c4bde7ce5c8a9ff4982
Wijdan Alqarni <wjdanalbdalzyz@gmail.com>
2025-03-22 02:17:20 +0200

    Add type hints and redirect for authenticated users in signup and login views

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+from django.http import HttpRequest
 from django.shortcuts import redirect, render
 
 from .forms import LoginForm, SignupForm
 
 
-def signup_view(request):
+def signup_view(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect('recommendations')
+    
     if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
@@ -13,13 +16,19 @@ def signup_view(request):
             user.set_password(form.cleaned_data["password"])
             user.save()
             login(request, user)
-            return redirect("recommendations")  # Change "home" to your desired redirect page
+            return redirect(
+                "recommendations"
+            )  # Change "home" to your desired redirect page
     else:
         form = SignupForm()
-    
+
     return render(request, "accounts/signup.html", {"form": form})
 
-def login_view(request):
+
+def login_view(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect('recommendations')
+
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -28,11 +37,14 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user:
                 login(request, user)
-                return redirect("recommendations")  # Change "home" to your desired redirect page
+                return redirect(
+                    "recommendations"
+                )  # Change "home" to your desired redirect page
     else:
         form = LoginForm()
-    
+
     return render(request, "accounts/login.html", {"form": form})
+
 
 def logout_view(request):
     logout(request)
--- base/views.py
+++ base/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
 from django.shortcuts import render
 
 
-def landing_page(request):
+def landing_page(request: HttpRequest):
     return render(request, "base/landing.html")
 
 
 @login_required
-def recommendations_page(request):
+def recommendations_page(request: HttpRequest):
     return render(request, "base/recommendations.html")
