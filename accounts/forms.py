ef198332cbb3b97dc2dffcf3e7abff3470ffd98c
Wijdan Alqarni <wjdanalbdalzyz@gmail.com>
2025-03-30 23:16:05 +0200

    bug fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, get_user_model
+User = get_user_model()
 
 
 class SignupForm(forms.ModelForm):
