bb853cee200d32803835e6349188fd7f6c805d38
Wijdan Alqarni <wjdanalbdalzyz@gmail.com>
2025-03-31 17:42:38 +0200

    bug fix

--- community/urls.py
+++ community/urls.py
@@ -6,9 +6,9 @@ from .views import community_reviews
 urlpatterns = [
     path("", community_reviews, name="community_reviews"),
     # API
-    path("api/reviews", api.community_reviews, name="community_reviews"),
-    path("api/places", api.places_list, name="places_list"),
-    path("api/review/add", api.add_review, name="add_review"),
-    path("api/review/<int:id>/reply", api.add_reply, name="add_reply"),
-    path("api/review/<int:id>/like", api.like_review, name="like_review"),
+    path("api/reviews", api.community_reviews, name="community_reviews_api"),
+    path("api/places", api.places_list, name="places_list_api"),
+    path("api/review/add", api.add_review, name="add_review_api"),
+    path("api/review/<int:id>/reply", api.add_reply, name="add_reply_api"),
+    path("api/review/<int:id>/like", api.like_review, name="like_review_api"),
 ]
