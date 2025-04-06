98fe3b6cc85d1e24483cbd81650b99912a644f13
Wijdan Alqarni <wjdanalbdalzyz@gmail.com>
2025-04-06 15:39:12 +0200

    Add email settings to production configuration and update landing page styles

--- green_haven/settings/production.py
+++ green_haven/settings/production.py
@@ -15,3 +15,12 @@ if not DEBUG:
         "API_SECRET": env("API_SECRET"),
     }
     DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+    # Email setting
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = env("EMAIL_HOST")
+    EMAIL_PORT = env("EMAIL_PORT")
+    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
