dffd43045470e381a2e8f3cc5d321c1fb2623a5e
Wijdan Alqarni <wjdanalbdalzyz@gmail.com>
2025-04-06 12:11:49 +0200

    updates

--- green_haven/settings/production.py
+++ green_haven/settings/production.py
@@ -1,16 +1,17 @@
 from .base import DEBUG, MIDDLEWARE, env
 
-ADMINS = [
-    ("Admin", env("ADMIN_EMAIL") ),
-]
-    
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+if not DEBUG:
+    ADMINS = [
+        ("Admin", env("ADMIN_EMAIL") ),
+    ]
+      
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+    MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
-# Cloudinary Settings For Media Files
-CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": env("CLOUD_NAME"),
-    "API_KEY": env("API_KEY"),
-    "API_SECRET": env("API_SECRET"),
-}
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+    # Cloudinary Settings For Media Files
+    CLOUDINARY_STORAGE = {
+        "CLOUD_NAME": env("CLOUD_NAME"),
+        "API_KEY": env("API_KEY"),
+        "API_SECRET": env("API_SECRET"),
+    }
+    DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
