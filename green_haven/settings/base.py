98bc19f7408cc92b7982dba92a908562414286a1
Wijdan Alqarni <wjdanalbdalzyz@gmail.com>
2025-04-06 12:05:01 +0200

    updates

--- green_haven/settings/base.py
+++ green_haven/settings/base.py
@@ -147,7 +147,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 
 PEXELS_API_KEY = env("PEXELS_API_KEY")
